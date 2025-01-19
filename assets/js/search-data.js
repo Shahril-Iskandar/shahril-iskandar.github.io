d07487cd29fd8d7929eedbe99f4f6f0bb6560487
Shahril-Iskandar <52953558+Shahril-Iskandar@users.noreply.github.com>
2025-01-19 14:16:06 +0000

    Deploying to gh-pages from @ Shahril-Iskandar/shahril-iskandar.github.io@e47f1ab92cc5aabbb6be421651ea75adfe61e551 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -42,7 +42,7 @@ ninja.data = [{
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/CV/";
+            window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
           title: "teaching",
